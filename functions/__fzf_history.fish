e32fb6e8f8d13933c17bd4397caa9233d4d3c420
Shinnosuke Sawada <6warashi9@gmail.com>
2018-09-13 20:38:57 +0900

    historyをsyncできるようにした

--- functions/__fzf_history.fish
+++ functions/__fzf_history.fish
@@ -1,5 +1,11 @@
 function __fzf_history
-    history | eval (__fzfcmd) --tac --no-sort --tiebreak=index | read -l select
+    set -q FZF_HISTORY_SYNC; or set FZF_HISTORY_SYNC 0
+    if [ $FZF_HISTORY_SYNC -eq 1 ]
+        history --save
+        history --merge
+    end
+
+    history | eval (__fzfcmd) --tiebreak=index | read -l select
     if not test -z "$select"
         commandline -rb $select
     end
