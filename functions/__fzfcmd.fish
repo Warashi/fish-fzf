2536282536dac94e2343eb3b9180b9da645bb802
Shinnosuke Sawada <6warashi9@gmail.com>
2019-12-25 18:52:38 +0900

    fix bug

--- functions/__fzfcmd.fish
+++ functions/__fzfcmd.fish
@@ -1,5 +1,5 @@
 function __fzfcmd
     set -q FZF_CMD; or set FZF_CMD "fzf-tmux"
     set -q FZF_DEFAULT_OPTIONS; or set FZF_DEFAULT_OPTIONS ""
-    echo $FZF_CMD $FZF_DEFAULT_OPTS
+    echo $FZF_CMD $FZF_DEFAULT_OPTIONS
 end
